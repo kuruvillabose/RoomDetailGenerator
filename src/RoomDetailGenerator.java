382d4fb9d295e30391f94fca62122324f5615d86
kuruvillabose <kuruvillamb@gmail.com>
2019-04-23 12:57:32 -0400

    second commit

--- src/RoomDetailGenerator.java
+++ src/RoomDetailGenerator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RoomDetailGenerator {
 
 	public static void main(String[] args) {
-		double length, width, area, perimeter;
+		double length, width,height, area, perimeter,volume;
 		String prompt;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome to Grand Circus' Room Detail Generator!");
@@ -14,10 +14,16 @@ public class RoomDetailGenerator {
 			length = scan.nextDouble();
 			System.out.println("Enter Width :");
 			width = scan.nextDouble();
+			System.out.println("Enter Height :");
+			height = scan.nextDouble();
+			
 			area = length * width;
 			perimeter = 2 * (length + width);
+			volume=length*width*height;
+			
 			System.out.println("Area :" + area);
 			System.out.println("Perimeter :" + perimeter);
+			System.out.println("Volume :" + volume);
 			System.out.println("");
 			System.out.println("Continue?(y/n):");
 			System.out.println("");
